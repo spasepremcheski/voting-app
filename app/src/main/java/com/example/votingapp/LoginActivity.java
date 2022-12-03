154ea0fb424b1abe19dd820301c76ab5cca0cada
spasepremcheski <spase0706@gmail.com>
2022-12-03 14:06:32 +0100

    Make onLoginButtonClick function

--- app/src/main/java/com/example/votingapp/LoginActivity.java
+++ app/src/main/java/com/example/votingapp/LoginActivity.java
@@ -3,12 +3,31 @@ package com.example.votingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.votingapp.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+
+    private void onLoginButtonClick(){
+        String username = binding.usernameEditText.getText().toString();
+        String password = binding.passwordEditText.getText().toString();
+
+        if(!username.isEmpty() && !password.isEmpty()) {
+            // login
+        }
+        else {
+            Toast.makeText(this, "Please login", Toast.LENGTH_SHORT).show();
+        }
     }
 }
