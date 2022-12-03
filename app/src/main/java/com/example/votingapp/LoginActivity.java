042fd8a7fac283624f380550d9ba4d5195af97bd
spasepremcheski <spase0706@gmail.com>
2022-12-03 14:30:00 +0100

    Make register screen

--- app/src/main/java/com/example/votingapp/LoginActivity.java
+++ app/src/main/java/com/example/votingapp/LoginActivity.java
@@ -2,32 +2,51 @@ package com.example.votingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.votingapp.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
     private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
 
+        binding.registerNavigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onRegisterNavigateButtonClick();
+            }
+        });
 
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLoginButtonClick();
+            }
+        });
     }
 
-    private void onLoginButtonClick(){
+    private void onLoginButtonClick() {
         String username = binding.usernameEditText.getText().toString();
         String password = binding.passwordEditText.getText().toString();
 
-        if(!username.isEmpty() && !password.isEmpty()) {
+        if (!username.isEmpty() && !password.isEmpty()) {
             // login
-        }
-        else {
+        } else {
             Toast.makeText(this, "Please login", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void onRegisterNavigateButtonClick() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
