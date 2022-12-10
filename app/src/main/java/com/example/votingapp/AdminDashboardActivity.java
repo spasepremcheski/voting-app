c558767808d5573ba2eb1b4d0b779a5bf1d76417
spasepremcheski <spase0706@gmail.com>
2022-12-10 13:43:12 +0100

    Declare database variable in AdminDashboardActivity

--- app/src/main/java/com/example/votingapp/AdminDashboardActivity.java
+++ app/src/main/java/com/example/votingapp/AdminDashboardActivity.java
@@ -1,13 +1,17 @@
 package com.example.votingapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.example.votingapp.database.VotesDatabase;
 import com.example.votingapp.databinding.ActivityAdminDashboardBinding;
 import com.example.votingapp.databinding.DialogAddVoteBinding;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.concurrent.Executors;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,11 +23,15 @@ public class AdminDashboardActivity extends AppCompatActivity {
 
     private VotesAdapter adapter;
 
+    VotesDatabase database;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAdminDashboardBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        database = ((Application) getApplication()).database;
 
         addNewVote();
         initRecyclerView();
@@ -59,6 +67,24 @@ public class AdminDashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // TODO: start the vote (check empty fields, save it to database, send notification)
+                String question = bottomSheetBinding.voteQuestionEditText.getText().toString();
+                String choiceOne = bottomSheetBinding.choiceOneEditText.getText().toString();
+                String choiceTwo = bottomSheetBinding.choiceTwoEditText.getText().toString();
+                String choiceThree = bottomSheetBinding.choiceThreeEditText.getText().toString();
+                String choiceFour =  bottomSheetBinding.choiceFourEditText.getText().toString();
+                String choiceFive = bottomSheetBinding.choiceFiveEditText.getText().toString();
+                String endTime = bottomSheetBinding.endTimeEditText.getText().toString();
+
+                Log.d("Time", String.valueOf(Calendar.getInstance().getTime()).substring(11,19));
+
+                if(!question.isEmpty()) {
+                    Executors.newSingleThreadExecutor().execute(new Runnable() {
+                        @Override
+                        public void run() {
+//                            database.votesDao().addVote(question, choiceOne, choiceTwo, choiceThree,choiceFour,choiceFive, );
+                        }
+                    });
+                }
 
                 dialog.dismiss();
             }
