edbdbf11700b8f9ddaf65b3a98b88a8fcce95b31
spasepremcheski <spase0706@gmail.com>
2022-12-10 19:32:22 +0100

    Design UserVotes and UserResults Fragments and put them in one UserActivity

---
+++ app/src/main/java/com/example/votingapp/UserResultsFragment.java
@@ -0,0 +1,23 @@
+package com.example.votingapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.votingapp.databinding.FragmentUserResultsBinding;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class UserResultsFragment extends Fragment {
+    private FragmentUserResultsBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentUserResultsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
